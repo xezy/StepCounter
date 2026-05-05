c092434a3e16c66cd66186aa9f3dcb93bb10f265
Eliyahu_Shwartz <eli.shwartz@gmail.com>
2026-05-05 13:14:15 +0200

    bug fixes

--- gradlew.bat
+++ gradlew.bat
@@ -27,22 +27,22 @@ goto fail
 
 set DIR=%~dp0
 set JAVA_EXE=%JAVA_HOME%\bin\java.exe
-set GRADLE_WRAPPER_JAR="%DIR%\gradle\wrapper\gradle-wrapper.jar"
-set GRADLE_WRAPPER_PROPERTIES="%DIR%\gradle\wrapper\gradle-wrapper.properties"
+set GRADLE_WRAPPER_JAR=%DIR%\gradle\wrapper\gradle-wrapper.jar
+set GRADLE_WRAPPER_PROPERTIES=%DIR%\gradle\wrapper\gradle-wrapper.properties
 
-if not exist %GRADLE_WRAPPER_JAR% goto downloadWrapper
+if not exist "%GRADLE_WRAPPER_JAR%" goto downloadWrapper
 goto run
 
 :downloadWrapper
 echo Downloading Gradle wrapper...
 powershell -Command "& {Invoke-WebRequest -Uri 'https://github.com/gradle/gradle/raw/v8.2.0/gradle/wrapper/gradle-wrapper.jar' -OutFile '%DIR%\gradle\wrapper\gradle-wrapper.jar'}"
-if not exist %GRADLE_WRAPPER_JAR% (
+if not exist "%GRADLE_WRAPPER_JAR%" (
     echo Failed to download gradle-wrapper.jar
     goto fail
 )
 
 :run
-%JAVA_EXE% -jar %GRADLE_WRAPPER_JAR% %*
+"%JAVA_EXE%" -jar "%GRADLE_WRAPPER_JAR%" %*
 goto end
 
 :fail
